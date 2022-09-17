af0b5d051a1d5d559fc7f2604c3ac308490045c1
devmcdev <97288649+devmcdev@users.noreply.github.com>
2022-09-17 12:03:39 +0100

    improved performace of events table

---
+++ TimeUtil.components/Shared/DataOutput/EventsTableData.cs
@@ -0,0 +1,12 @@
+﻿using TimeUtil.Shared;
+
+namespace TimeUtil.Components.Shared.DataOutput;
+public sealed class EventsTableData
+{
+    public EventsTableData(IEnumerable<Event> events)
+    {
+        Events = events;
+    }
+
+    public IEnumerable<Event> Events { get; }
+}
